71e9707b28a91ffdd272f8ab93a97ce789581255
Carlos Jiménez <cojimene@gmail.com>
2017-11-04 22:27:50 -0500

    changes logo

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -6,7 +6,7 @@ class UserProfile < ApplicationRecord
   belongs_to :user
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment :avatar, content_type: { content_type: %w[image/jpeg image/png image/gif] }, size: { in: 0..5.megabytes }
+  validates_attachment :avatar, presence: true, content_type: { content_type: %w[image/jpeg image/png image/gif] }, size: { in: 0..5.megabytes }
 
   validates :user, :first_name, :last_name, :birthdate, :about, :experience, :city, :phone, :role, presence: true
   validates :role, inclusion: { in: ROLES, allow_blank: true }
